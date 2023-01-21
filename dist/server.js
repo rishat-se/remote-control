84a5f59d89a22c5a87dfd315615c45bd298fbe42
Rishat Agzamov <rishat.se@gmail.com>
2023-01-21 00:19:51 -0500

    feat: basic websocket server

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,8 @@
-"use strict";
-console.log('Hello, world!');
+import { WebSocketServer } from 'ws';
+const wss = new WebSocketServer({ port: 8080 });
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+    ws.send('something');
+});
